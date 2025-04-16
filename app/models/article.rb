603f762fb7a26420933405a2808fc69253cd626b
Guilherme Vieira <gv@cfgroup.com.br>
2025-04-16 10:06:30 -0300

    aa

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,13 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
-  validates :title, presence: true,
-                    length: { minimum: 5 }
+  validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :text, length: {maximum: 1000}
+  before_validation :capitalize_title
+
+  def capitalize_title
+    return if title.blank?
+  
+    self.title = title.strip.sub(/\A\w/) { |c| c.upcase }
+  end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :article
-  validates :commenter, presence: true
+  validates :commenter, presence: true, length: {maximum: 50}
+  validates :body, length: {maximum: 500}
 end
